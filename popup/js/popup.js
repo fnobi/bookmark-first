bb84e74e4e92f1cff5ee9e87dfee5a24d57d9533
fujisawa-shin <fujisawa-shin@kayac.com>
2017-02-15 23:26:36 +0900

    方向キーでの選択ができるように

--- popup/js/popup.js
+++ popup/js/popup.js
@@ -1,18 +1,56 @@
+const KEYCODE_ENTER = 13;
+const KEYCODE_UP = 38;
+const KEYCODE_DOWN = 40;
+
 const incrementDom = document.querySelector('.js-increment');
 const bookmarkRootDom = document.querySelector('.js-bookmark-root');
 
 let rootBookmark = null;
+let activeIndex = 0;
+let isCursorKey = false;
 
 function init () {
-    initKeyEvent();
+    initIncrementEvent();
     initAnchorClickEvent();
     loadBookmark();
 }
 
-function initKeyEvent () {
+function initIncrementEvent () {
     incrementDom.addEventListener('keyup', () => {
+        if (isCursorKey) {
+            isCursorKey = false;
+            return;
+        }
         const globalMatch = rootBookmark.match(incrementDom.value);
         bookmarkRootDom.setAttribute('data-empty', !globalMatch);
+        setActive(0);
+    });
+    
+    document.addEventListener('keydown', (e) => {
+        switch (e.keyCode) {
+        case KEYCODE_ENTER:
+            const activeAnchor = document.querySelector('a[data-active="true"]');
+            if (activeAnchor && activeAnchor.href) {
+                window.open(activeAnchor.href);
+            }
+            break;
+        case KEYCODE_UP:
+            setActive(activeIndex - 1);
+            isCursorKey = true;
+            break;
+        case KEYCODE_DOWN:
+            setActive(activeIndex + 1);
+            isCursorKey = true;
+            break;
+        }
+    });
+    
+    incrementDom.addEventListener('blur', () => {
+        clearActive();
+    });
+    
+    incrementDom.addEventListener('focus', () => {
+        setActive(0);
     });
 }
 
@@ -35,6 +73,25 @@ function loadBookmark () {
 
         bookmarkRootDom.appendChild(rootBookmark.itemDom);
     });
+}
+
+function clearActive () {
+    document.querySelectorAll('a[data-active="true"]').forEach((el) => {
+        el.setAttribute('data-active', false);
+    });
+}
+
+function setActive (index) {
+    clearActive();
+    const matching = document.querySelectorAll('li[data-match="true"] > a');
+    if (!matching.length) {
+        return;
+    }
+
+    index = Math.max(index, 0);
+    index = Math.min(index, matching.length - 1);
+    matching[index].setAttribute('data-active', true);
+    activeIndex = index;
 }
 
 init();
